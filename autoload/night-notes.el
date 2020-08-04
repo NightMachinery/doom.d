591be4924e6b545e4e43699194da7e7b3a2349a0
NightMachinary <rudiwillalwaysloveyou@gmail.com>
2020-08-04 17:31:27 +0430

    mr

--- autoload/night-notes.el
+++ autoload/night-notes.el
@@ -25,9 +25,11 @@
     (message cmd)
     ;; (message link)
     ;; (insert (progn (setenv  "brishzr_in" link) (shell-command-to-string "brishzr.dash inargsf unt")))
-    (insert (shell-command-to-string cmd))))
+    (insert (shell-command-to-string cmd))
+    (save-buffer)
+    ))
 
 (night/set-leader-keys " z l" #'night/unt)
 ;; (message (shell-command-to-string "brishz.dash unt"))
 ;; (message (progn (setenv  "brishzr_in" "haki") (shell-command-to-string "echo $brishzr_in")))
-;;;
+;;; 
