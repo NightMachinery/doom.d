b76aae84892e78d784bbc72774165d5ec5fc3db7
NightMachinary <rudiwillalwaysloveyou@gmail.com>
2020-08-05 18:14:25 +0430

    .

--- autoload/night-notes.el
+++ autoload/night-notes.el
@@ -12,20 +12,42 @@
   ;; fzf seems slower, but it supports fzf syntax and is async
   ;; (counsel-fzf "" (getenv "nightNotes"))
   ;; (dired (getenv "nightNotes"))
-)
+  )
 (night/set-leader-keys " z ." #'night/browse-notes)
 
 ;;;
+(comment (defun night/unt-async ()
+           (interactive)
+           (let* (
+                  (link (current-kill 0))
+                  ;; (my-buffer (current-buffer))
+                  )
+             (async-start (lambda () (let* (
+                                            (cmd (concat "brishzr.dash " (shell-quote-argument (concat "ec " (shell-quote-argument link) " | inargsf unt"))))
+                                            (text (shell-command-to-string cmd))
+                                            )
+                                       text
+                                       ))
+                          (lambda (text)
+                            (message "name: %s my-buffer: %s string: %s" (buffer-name) "my-buffer" (buffer-string))
+                            ;; (with-current-buffer my-buffer (insert text))
+                            )))))
+
 (defun night/unt ()
   (interactive)
-  (let* ((link (current-kill 0))
+  (let* (
+         (my-column (current-column))
+         (link (current-kill 0))
          ;; (cmd (concat "brishzr.dash unt " (shell-quote-argument link)))
          (cmd (concat "brishzr.dash " (shell-quote-argument (concat "ec " (shell-quote-argument link) " | inargsf unt"))))
+         (text (progn
+                 (message "%s" cmd)
+                 (shell-command-to-string cmd)))
          )
-    (message "%s" cmd)
-    ;; (message "%s" link)
-    ;; (insert (progn (setenv  "brishzr_in" link) (shell-command-to-string "brishzr.dash inargsf unt")))
-    (insert (shell-command-to-string cmd))
+    (dolist (line (split-string text "\n"))
+      (insert line "\n")
+      (dotimes (i my-column)
+        (insert " ")))
     (save-buffer)
     ))
 
--- autoload/night-pdf.el
+++ autoload/night-pdf.el
@@ -13,20 +13,20 @@
 
 (setq-default night/pdf-scroll-step 30)
 
-(defun evil-collection-pdf-view-next-line-or-next-page (&optional count)
-  "'evil' wrapper include a count argument to `pdf-view-next-line-or-next-page'"
-  (interactive "P")
-  (if count
-      ;; (dotimes (_ count nil)
-      ;; (pdf-view-next-line-or-next-page 1))
-      (pdf-view-next-line-or-next-page count)
-    (pdf-view-next-line-or-next-page night/pdf-scroll-step)))
-
-(defun evil-collection-pdf-view-previous-line-or-previous-page (&optional count)
-  "'evil' wrapper include a count argument to `pdf-view-previous-line-or-previous-page'"
-  (interactive "P")
-  (if count
-      ;; (dotimes (_ count nil)
-      ;; (pdf-view-previous-line-or-previous-page 1))
-      (pdf-view-previous-line-or-previous-page count)
-    (pdf-view-previous-line-or-previous-page night/pdf-scroll-step)))
+(after! evil-collection (progn (defun evil-collection-pdf-view-next-line-or-next-page (&optional count)
+                                 "'evil' wrapper include a count argument to `pdf-view-next-line-or-next-page'"
+                                 (interactive "P")
+                                 (if count
+                                     ;; (dotimes (_ count nil)
+                                     ;; (pdf-view-next-line-or-next-page 1))
+                                     (pdf-view-next-line-or-next-page count)
+                                   (pdf-view-next-line-or-next-page night/pdf-scroll-step)))
+                               (defun evil-collection-pdf-view-previous-line-or-previous-page (&optional count)
+                                 "'evil' wrapper include a count argument to `pdf-view-previous-line-or-previous-page'"
+                                 (interactive "P")
+                                 (if count
+                                     ;; (dotimes (_ count nil)
+                                     ;; (pdf-view-previous-line-or-previous-page 1))
+                                     (pdf-view-previous-line-or-previous-page count)
+                                   (pdf-view-previous-line-or-previous-page night/pdf-scroll-step)))
+                               ))
