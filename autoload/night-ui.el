88e0860aa0110a3150a733ae3013223715af2298
NightMachinary <rudiwillalwaysloveyou@gmail.com>
2020-08-13 01:33:30 +0430

    mr

--- autoload/night-ui.el
+++ autoload/night-ui.el
@@ -5,11 +5,15 @@
 (defun night/highlight-atsign ()
   (interactive)
   (font-lock-add-keywords
-   nil '(("\\(@[^[:space:][:punct:]\n]+\\)" 1 'special-comment t))))
+   nil '(("\\B\\(@[^[:space:][:punct:]\n]+\\)" 1 'special-comment t))))
+;;; tests
+;; jas (@wsw aws) hi@gmail.com
+;;a@a
+;;@hi
+;;;
 (defun night/generic-hook-fn ()
   (interactive)
   ;; (flyspell-mode-off)
-  (night/highlight-atsign))
+  (night/highlight-atsign)  ; @bug This hook might get called multiple times on a single buffer, and that can cause pollution.
+  )
 (add-hook 'after-change-major-mode-hook #'night/generic-hook-fn)
-;; jas (@wsw aws)
-;;
--- init.el
+++ init.el
@@ -31,8 +31,8 @@
        ;; doom-quit         ; DOOM quit-message prompts when you quit Emacs
        ;;fill-column       ; a `fill-column' indicator
        hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
-       ;;hydra
-       ;;indent-guides     ; highlighted indent columns
+       hydra
+       indent-guides     ; highlighted indent columns
        ;;minimap           ; show a map of the code on the side
        modeline          ; snazzy, Atom-inspired modeline, plus API
        nav-flash         ; blink cursor line after big motions
