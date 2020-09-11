a5e2439c3d64eed463259c3bd19bf4afe985460e
NightMachinary <rudiwillalwaysloveyou@gmail.com>
2020-09-11 14:00:16 +0430

    .

--- autoload/org/templates.el
+++ autoload/org/templates.el
@@ -3,7 +3,8 @@
 ;;;
 ;; The new API is very limited and can only insert #+BEGIN_X and #+END_X
 (require 'org-tempo)
-(add-to-list 'org-structure-template-alist '("sj" . "src julia"))
+;; (add-to-list 'org-structure-template-alist '("sj" . "src julia"))
+(add-to-list 'org-structure-template-alist '("sj" . "src jupyter-julia :session j1"))
 (add-to-list 'org-structure-template-alist '("sb" . "src bash :results verbatim :exports both"))
 (add-to-list 'org-structure-template-alist '("sp" . "src python :session p1 :results value :exports both"))
 (add-to-list 'org-structure-template-alist '("spo" . "src python :session p1 :results output :exports both"))
