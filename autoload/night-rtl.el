b757ebf6b0178528db079b0dcd8908ea137a9cd0
NightMachinary <rudiwillalwaysloveyou@gmail.com>
2020-09-22 11:17:18 +0330

    .

---
+++ autoload/night-rtl.el
@@ -0,0 +1,20 @@
+;;; autoload/night-rtl.el -*- lexical-binding: t; -*-
+
+(setq night/persian-font "B Nazanin") ;; "IranNastaliq"
+
+(defun night/enable-bidirectional ()
+  "interactive"
+  (set-fontset-font
+   "fontset-default"
+   ;; http://www.gnu.org/software/emacs/manual/html_node/emacs/Modifying-Fontsets.html
+   ;; https://unicodemap.org/search.asp?search=%D9%84%DA%AF%DA%86%D9%BE%DA%98
+   ;; https://en.wikipedia.org/wiki/Persian_alphabet
+   ;; `describe-char`
+   ;; (cons (decode-char 'ucs #x0600) (decode-char 'ucs #
+   ;; arabic (includes Persian) range
+   'arabic
+   night/persian-font
+   )
+  (setq face-font-rescale-alist `((,night/persian-font . 1.50)))
+  (setq bidi-paragraph-direction 'nil))
+(add-hook 'org-mode-hook 'night/enable-bidirectional)
