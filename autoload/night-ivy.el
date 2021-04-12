8bbc5ae2227a9c179d5fe1b499b59ae31f9535f1
NightMachinary <rudiwillalwaysloveyou@gmail.com>
2021-04-13 02:39:29 +0430

    fixed bug (M-x polluting next ivy-read) in counsel fzf functions; added night/counsel-clipboard; added ivy-mark helpers and hotkeys

--- autoload/night-ivy.el
+++ autoload/night-ivy.el
@@ -9,12 +9,12 @@
 
 (after! (ivy  counsel)
   (defun night/ivy--directory-out ()
-  (let (dir)
-    (when (and
-           ;; instead of ivy--directory: (not as good) (ivy-state-current ivy-last)
-           (setq dir (ivy-expand-file-if-directory (concat ivy--directory "/../"))))
-      (ivy--cd dir)
-      (ivy--exhibit))))
+    (let (dir)
+      (when (and
+             ;; instead of ivy--directory: (not as good) (ivy-state-current ivy-last)
+             (setq dir (ivy-expand-file-if-directory (concat ivy--directory "/../"))))
+        (ivy--cd dir)
+        (ivy--exhibit))))
 
   (define-key counsel-find-file-map (kbd "<left>")
     (lambda ()
@@ -26,4 +26,27 @@
       (interactive)
       (ivy--directory-enter)
       ))
+  ;;;
+  (defun night/ivy-mark-toggle ()
+    "Mark/unmark the selected candidate."
+    (interactive)
+    (if (ivy--marked-p)
+        (ivy--unmark (ivy-state-current ivy-last))
+      (ivy--mark (ivy-state-current ivy-last)))
+    )
+  (defun night/ivy-mark-toggle-up ()
+    (interactive)
+    (night/ivy-mark-toggle)
+    (ivy-previous-line))
+  (defun night/ivy-mark-toggle-down ()
+    (interactive)
+    (night/ivy-mark-toggle)
+    (ivy-next-line))
+  ;; @rememberMe
+  (define-key ivy-minibuffer-map (kbd "TAB") #'night/ivy-mark-toggle)
+  (define-key ivy-minibuffer-map (kbd "S-<up>") 'night/ivy-mark-toggle-up)
+  (define-key ivy-minibuffer-map (kbd "S-<down>") 'night/ivy-mark-toggle-down)
+  ;; (define-key ivy-minibuffer-map (kbd "S-TAB") 'ivy-unmark)
+  ;; (define-key ivy-minibuffer-map (kbd "<backtab>") 'ivy-unmark)
+  ;;;
   )
