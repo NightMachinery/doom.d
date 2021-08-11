b4bec9b9bdfeede6f7f8cefc8ef95fb0caa44ea8
NightMachinary <rudiwillalwaysloveyou@gmail.com>
2021-08-11 16:16:29 +0430

    modified: night-protocol.el

--- autoload/org/night-protocol.el
+++ autoload/org/night-protocol.el
@@ -1,23 +1,25 @@
 ;;; autoload/org/night-protocol.el -*- lexical-binding: t; -*-
 
-(setq org-html-validation-link nil)  ;; removes validation link from exported html file
-(require 'org-protocol)
-(defun transform-square-brackets-to-round-ones(string-to-transform)
-  "Transforms [ into ( and ] into ), other chars left unchanged."
-  (concat
-   (mapcar #'(lambda (c) (if (equal c ?\[) ?\( (if (equal c ?\]) ?\) c))) string-to-transform))
+(after! night-roam
+  (setq org-html-validation-link nil) ;; removes validation link from exported html file
+  (require 'org-protocol)
+  (defun transform-square-brackets-to-round-ones(string-to-transform)
+    "Transforms [ into ( and ] into ), other chars left unchanged."
+    (concat
+     (mapcar #'(lambda (c) (if (equal c ?\[) ?\( (if (equal c ?\]) ?\) c))) string-to-transform))
+    )
+  (setq org-capture-templates
+        `(
+          ("p" "Protocol" entry (file+headline ,(concat (getenv "nightNotes") "/org/inbox.org") "URLs")
+           ;; "* %^{Title}\nSource: %u, %c\n #+BEGIN_QUOTE\n%i\n#+END_QUOTE\n\n\n%?"
+           "* %a\n #+BEGIN_QUOTE\n%i\n#+END_QUOTE\n%?"
+           )
+          ("L" "Protocol Link" entry (file+headline ,(concat (getenv "nightNotes") "/org/inbox.org") "URLs")
+           "* %?[[%:link][%(transform-square-brackets-to-round-ones \"%:description\")]]\n")
+          ("o" "Link capture" entry
+           (file+headline ,(concat (getenv "nightNotes") "/org/inbox.org") "URLs")
+           "* %a"                       ; %U timestamp
+           :immediate-finish t)
+          ))
+  ;; (setq org-protocol-default-template-key "o")
   )
-(setq org-capture-templates
-      `(
-        ("p" "Protocol" entry (file+headline ,(concat (getenv "nightNotes") "/org/inbox.org") "URLs")
-                  ;; "* %^{Title}\nSource: %u, %c\n #+BEGIN_QUOTE\n%i\n#+END_QUOTE\n\n\n%?"
-                  "* %a\n #+BEGIN_QUOTE\n%i\n#+END_QUOTE\n%?"
-         )
-        ("L" "Protocol Link" entry (file+headline ,(concat (getenv "nightNotes") "/org/inbox.org") "URLs")
-         "* %?[[%:link][%(transform-square-brackets-to-round-ones \"%:description\")]]\n")
-        ("o" "Link capture" entry
-         (file+headline ,(concat (getenv "nightNotes") "/org/inbox.org") "URLs")
-         "* %a"                         ; %U timestamp
-         :immediate-finish t)
-        ))
-;; (setq org-protocol-default-template-key "o")
