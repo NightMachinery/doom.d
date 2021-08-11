68b1f63b3195b73bd1151bbbf02a41d58f7cd0a8
NightMachinary <rudiwillalwaysloveyou@gmail.com>
2021-08-11 16:24:02 +0430

    modified: night-roam.el

--- autoload/org/night-roam.el
+++ autoload/org/night-roam.el
@@ -1,6 +1,8 @@
 ;;; autoload/org/night-roam.el -*- lexical-binding: t; -*-
 
-(after! (org evil-org evil org-roam)
+(after! (org-roam
+         ;; org evil-org evil ;; these seem redundant, and they interfere with the timely setting of org-roam-directory
+         )
   (cond
    ((zb isServer) (setq org-roam-directory (concat (getenv "HOME") "/org-roam/")))
    (nil (setq org-roam-directory (getenv "nightNotes"))
