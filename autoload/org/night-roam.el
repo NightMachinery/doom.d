67ae7afe3ec9eb8e3be03e54debd5e22bcbea246
NightMachinary <rudiwillalwaysloveyou@gmail.com>
2021-08-11 16:21:51 +0430

    modified: night-roam.el

--- autoload/org/night-roam.el
+++ autoload/org/night-roam.el
@@ -7,6 +7,7 @@
         ;; disabled because of performance issues
         )
    )
+  (z mkdir -p (i org-roam-directory))
 
   (setq +org-roam-open-buffer-on-find-file nil)
   (setq org-roam-db-update-idle-seconds 600)
