913b39245e529326ec5e3cb9c7e3d7edc692e6a5
NightMachinery <feraidoonmehri@gmail.com>
2023-09-14 13:25:02 +0330

    modified: night-avy.el

--- autoload/night-avy.el
+++ autoload/night-avy.el
@@ -24,28 +24,34 @@
     (interactive)
     ;; Get the starting position
     (save-excursion
-      (let ((start-char (read-char "Start char: "))
-            start-pos end-pos)
-        (setq start-pos (avy-process (avy--regex-candidates (string start-char))))
-        ;; If a starting position is selected
-        (when start-pos
-          (let ((end-char (read-char "End char: ")))
-            (setq end-pos (avy-process (avy--regex-candidates (string end-char))))
-            ;; If an ending position is selected, copy the text
-            (when end-pos
-              ;; (message "start: %s, end: %s" start-pos end-pos)
-              ;; start: (3417 . 3418), end: (3419 . 3420)
-              (let ((start-pos (car start-pos))
-                    (end-pos (cdr end-pos)))
-                (kill-ring-save
-                 start-pos
-                 end-pos)
-                (nav-flash-show start-pos end-pos 'urgent-face 1))))))))
+      (when-let ((start-char (night/read-char-or-cancel "Start char: ")))
+        ;; (message "start-char: %s" start-char)
+          (let (start-pos end-pos)
+            (setq start-pos (avy-process (avy--regex-candidates (string start-char))))
+            ;; If a starting position is selected
+            (when start-pos
+              (when-let ((end-char (night/read-char-or-cancel "End char: ")))
+                (setq end-pos (avy-process (avy--regex-candidates (string end-char))))
+                ;; If an ending position is selected, copy the text
+                (when end-pos
+                  ;; (message "start: %s, end: %s" start-pos end-pos)
+                  ;; start: (3417 . 3418), end: (3419 . 3420)
+                  (let ((start-pos (car start-pos))
+                        (end-pos (cdr end-pos)))
+                    (kill-ring-save
+                     start-pos
+                     end-pos)
+                    (nav-flash-show start-pos end-pos 'urgent-face 1)))))))))
   (map!
    :nvo
-   "g y" #'night/avy-copy-from-char-to-char)
-  (map!
-   :map magit-mode-map
-   :nvo
-   "g y" #'night/avy-copy-from-char-to-char)
+   "g y" #'night/avy-copy-from-char-to-char
+   :leader
+   "y a" #'night/avy-copy-from-char-to-char)
+  (after! magit
+    (map!
+     :map magit-mode-map
+     :nvo
+     "g y" #'night/avy-copy-from-char-to-char)
+    ;; [[https://github.com/emacs-evil/evil-collection/issues/754][{Q} How do I add a new hotkey? · Issue #754 · emacs-evil/evil-collection]]
+    )
   )
