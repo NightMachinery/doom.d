c06086ba594b8760fa9f36963b08b5ebf36853f2
NightMachinery <feraidoonmehri@gmail.com>
2024-10-10 16:01:51 +0330

    projectile: disabled caching and hybrid processing

--- autoload/night-projectile.el
+++ autoload/night-projectile.el
@@ -1,4 +1,9 @@
 ;;; autoload/night-projectile.el -*- lexical-binding: t; -*-
 
 (after! (projectile)
-  (setq projectile-dirconfig-comment-prefix "#"))
+  (setq projectile-dirconfig-comment-prefix "#")
+  ;;
+  (setq projectile-enable-caching nil)
+  ;; (setq projectile-indexing-method 'hybrid)
+  (setq projectile-indexing-method 'alien)
+  )
