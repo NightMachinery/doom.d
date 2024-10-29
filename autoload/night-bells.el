8c63e04f66c3d483a337722a23f750d5abf7236a
NightMachinery <feraidoonmehri@gmail.com>
2024-10-30 01:25:30 +0330

    added night/bell-visual-flash-buffer

--- autoload/night-bells.el
+++ autoload/night-bells.el
@@ -30,7 +30,24 @@
   (interactive)
   (z-async t bell-batman-cave-open))
 ;;;
+(defun night/bell-visual-flash-buffer ()
+  "Flash the current buffer with a blue background briefly."
+  (interactive)
+  (let ((flash-overlay (make-overlay (point-min) (point-max))))
+    (overlay-put flash-overlay 'face '(:background
+                                       ;; "plum"
+                                       ;; "pink"
+                                       "MediumPurple1"
+                                       :foreground "MintCream"
+                                       :extend t))  ; Make overlay extend past text
+    (overlay-put flash-overlay 'window (selected-window))  ; Ensure it applies to current window
+    (overlay-put flash-overlay 'priority 100)  ; High priority to ensure visibility
+    (run-with-timer 0.4 nil #'delete-overlay flash-overlay)))
+;;;
 (after! (org ox-beamer)
   ;; (advice-add #'org-beamer-export-to-pdf :after #'night/bell-export-completed)
+
+  ;; (night/unadvice #'org-babel-execute-subtree)
+  (advice-add #'org-babel-execute-subtree :after #'night/bell-visual-flash-buffer)
   )
 ;;;
