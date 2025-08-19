c853d15bb4b002fab45d527f4bab86be0b3b62d9
NightMachinery <feraidoonmehri@gmail.com>
2025-08-19 19:35:01 +0330

    feat(magit): support numeric diff ranges
    Advise magit-diff-range to handle numeric input.
    
    Format as night/magit-diff-numeric-prefix + N (default HEAD~).

--- autoload/night-magit.el
+++ autoload/night-magit.el
@@ -31,4 +31,29 @@
             (with-current-buffer "*Git Patch Output*"
               (error "Failed to apply patch:\n%s" (buffer-string)))))))))
 ;;;
+(defcustom night/magit-diff-numeric-prefix "HEAD~"
+  "The prefix to use when a numeric argument is given to `magit-diff`.
+The number entered by the user will be appended to this string."
+  :type 'string
+  :group 'magit)
+
+(defun night/h-magit-diff-range-advice (original-function &rest args)
+  "Advise `magit-diff-range' to handle numeric input as `night/magit-diff-numeric-prefix'N.
+When the user enters a number for the range, this function
+intercepts it and formats it according to
+`night/magit-diff-numeric-prefix' before passing it to the
+original `magit-diff-range' function."
+  (let ((verbosity-level 0)
+        (range (car args)))
+    (cond
+     ((and (stringp range) (string-match-p "\\`[0-9]+\\'" range))
+      (let ((new-range (concat night/magit-diff-numeric-prefix range)))
+        (when (> verbosity-level 0)
+          (message "Magit-diff: transformed numeric range '%s' to '%s'" range new-range))
+        (apply original-function (cons new-range (cdr args)))))
+     (t
+      (apply original-function args)))))
+
+(advice-add 'magit-diff-range :around #'night/h-magit-diff-range-advice)
+;;;
   )
